f4cf9251117a5485883d1dff5592c466c4ecb188
haryor0 <152572683+haryor0@users.noreply.github.com>
2023-12-01 01:23:49 +0000

    added necessary website files

---
+++ assets/js/extra.js
@@ -0,0 +1,65 @@
+function toggleReadMore() {
+    var moreContent = document.querySelector('.more-content');
+    var readMoreLink = document.querySelector('.read-more');
+
+    if (moreContent.style.display === 'none' || moreContent.style.display == ''){
+        moreContent.style.display = 'block';
+        readMoreLink.textContent = 'Read Less';
+    } else {
+        moreContent.style.display = 'none';
+        readMoreLink.textContent = 'Read More...';
+    }
+}
+
+
+function toggleReadMore2() {
+    var moreContent = document.querySelector('.more-content-2');
+    var readMoreLink = document.querySelector('.read-more-2');
+
+    if (moreContent.style.display === 'none' || moreContent.style.display == ''){
+        moreContent.style.display = 'block';
+        readMoreLink.textContent = 'Read Less';
+    } else {
+        moreContent.style.display = 'none';
+        readMoreLink.textContent = 'Read More...';
+    }
+}
+
+function toggleReadMore3() {
+    var moreContent = document.querySelector('.more-content-3');
+    var readMoreLink = document.querySelector('.read-more-3');
+
+    if (moreContent.style.display === 'none' || moreContent.style.display == ''){
+        moreContent.style.display = 'block';
+        readMoreLink.textContent = 'Read Less';
+    } else {
+        moreContent.style.display = 'none';
+        readMoreLink.textContent = 'Read More...';
+    }
+}
+
+function toggleReadMore4() {
+    var moreContent = document.querySelector('.more-content-4');
+    var readMoreLink = document.querySelector('.read-more-4');
+
+    if (moreContent.style.display === 'none' || moreContent.style.display == ''){
+        moreContent.style.display = 'block';
+        readMoreLink.textContent = 'Read Less';
+    } else {
+        moreContent.style.display = 'none';
+        readMoreLink.textContent = 'Read More...';
+    }
+}
+
+function toggleReadMore5() {
+    var moreContent = document.querySelector('.more-content-5');
+    var readMoreLink = document.querySelector('.read-more-5');
+
+    if (moreContent.style.display === 'none' || moreContent.style.display == ''){
+        moreContent.style.display = 'block';
+        readMoreLink.textContent = 'Read Less';
+    } else {
+        moreContent.style.display = 'none';
+        readMoreLink.textContent = 'Read More...';
+    }
+}
